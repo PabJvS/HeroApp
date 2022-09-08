2e5d63a62e88aceb6b7eb3955cbf9a88f649e69c
PabVera <pablojvs94@hotmail.com>
2022-09-08 00:02:09 -0500

    update heropage

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -16,7 +16,7 @@ export const HeroPage = () => {
       navigate(-1);
    };
 
-   const heroImageUrl = `./assets/heroes/${id}.jpg`;
+   const heroImageUrl = `../assets/heroes/${id}.jpg`;
 
    if (!hero) {
       return <Navigate to='/marvel' />;
